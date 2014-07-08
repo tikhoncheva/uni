cf3ea241a0b9821d0c0a5e1726063cb1c8243887
tikhoncheva <kasiatikhoncheva@gmail.com>
2014-07-08 19:23:03 +0200

    first working version

--- Objekterkennung/ObjectTracking/ObjectTracking.m
+++ Objekterkennung/ObjectTracking/ObjectTracking.m
@@ -14,23 +14,24 @@ imagefiles = dir([input_path filesep '*.jpg']) ;
 Nframes = length(imagefiles);   
 
 % then read all images from the order
-Nframes = 2;
+%Nframes = 50;
 frames = cell(1,Nframes); % cell of the images
 for i=1:Nframes
     currentfilename = imagefiles(i).name;
     frames{i} = imread([input_path filesep currentfilename]);
+    frames{i} = im2double(frames{i});
 end
 
 %
 %% 2: mark object to track 
 
-a = 25; % horizontal radius of Ellipse
-b = 30; % vertical radius of Ellipse
+a = 30; % horizontal radius of Ellipse
+b = 40; % vertical radius of Ellipse
 
 % show first image and wait till tracking object will be marked
 f = figure;
     imagesc(frames{1}),
-    title(sprintf('First frame from %i', Nframes));
+    title(sprintf('Frame 1 from %i', Nframes));
     
     % wait
     uiwait(msgbox('Locate the object! First center'));
@@ -44,119 +45,147 @@ f = figure;
     t=-pi:0.01:pi;
         xt=x0+a*cos(t);
         yt=y0+b*sin(t);
-    plot(xt,yt)
+    plot(xt,yt,'r')
 print(f, '-r80', '-dtiff', fullfile(output_path, sprintf('Frame1.jpg')));
 hold off;    
     
     
-    
-% frames{1} = imresize(frames{1}, [size(frames{1},1)*a ...
-%                                  size(frames{1},2)*b]);
-% x0 = x0*b;
-% y0 = y0*a;
-
-
-% f = figure;
-%     imagesc(frames{1}),
-%     title(sprintf('First frame from %i', Nframes));
-%     hold on;
-%     
-%     % plot an ellipse
-%     t=-pi:0.01:pi;
-%         xt=x0+a*cos(t);
-%         yt=y0+b*sin(t);
-%     plot(xt,yt)
-
-% print(f, '-r80', '-dtiff', fullfile(output_path, sprintf('Frame1.jpg')));
-% hold off;
-
 
 %% TRACKING
 
 %% Step 1: Initialization
 
-% center of the tracking object in each frame
-cObj = zeros(Nframes,2);
+% center of the target (CT) in each frame
+CT = zeros(Nframes,2);
+% normalized coordinates of target center in each frame
+normCT = zeros(Nframes,2);
+
 % initial location of the target (got from user)
-cObj(1,:) = [x0, y0];
+CT(1,:) = [x0, y0];
 %
 % Parameters
 % Feature Space : RGB color space quantized in 16x16x16 bins
 m = 16^3; % m-bins  histogram
-h = 100;  % kernel bandwidth
-eps = 0.001; % precision
+h = 2.0;  % kernel bandwidth
+eps = 0.0001; % precision
 
 %% Target Model
 
 % find pixel inside selected Ellipse
-x_star = findPixels(frames{1}, cObj(1,:), a, b);
-% % calculate delta(b(x_star_i) - u)
-% % because we don't need to save feature representations of x_star separately
-dbu = delta_b_u(frames{1}, x_star, m); %size: size(x_star,1) times m
+x= findPixels(frames{1}, CT(1,:), a, b);
 
+% because we don't need to save feature representations of x_star separately
+% calculate delta(b(x_star_i) - u)
+dbu = delta_b_u(frames{1}, x, m); %size: size(x_star,1) times m
+% normalized coordinates of pixels inside selected region and target center
+[normX, normCT(1,:)]= normalizeX(x,CT(1,:),a,b);
 
 % Target model: 
-%q = pdf(x_star, dbu, cObj(1,:), m, 1);
-q = pdf(x_star, dbu, [0 0], m, 1);
-q_pos = find(q>0)
-%% Algorithm
+q = pdf(normX, dbu, normCT(1,:) , m, 1.);
+
+
+%% Targen Lokalization!
 
 % for each frame
 for frameId=2:Nframes
-    cy0 = cObj(frameId-1,:);
-    % find center of the target :
-    cy1 = [0,0];
+    % get center of the previous frame
+    y0 = CT(frameId-1,:);
+    % find pixels of interest (POI) around y0
+    x= findPixels(frames{frameId}, y0, a, b);
+    % calculate delta(b(x_star_i) - u)
+    dbu = delta_b_u(frames{frameId}, x, m); 
+    % normalized coordinates of pixels inside selected region and target center
+    [normX, normy0] = normalizeX(x, y0, a, b);
+       
+    % Target candidates
+    p_y0 = pdf(normX, dbu , normy0, m , h);
+ 
+    % Bhattacharyya Coefficient
+    rho0 = sum(sqrt(q.*p_y0));
     
-%     frames{frameId} = imresize(frames{frameId}, [size(frames{frameId},1)*a ...
-%                                  size(frames{frameId},2)*b]);
-    count = 1;
-    while count~=2 %( sqrt((cy0-cy1)*(cy0-cy1)') > eps)
-        % find pixels of interest (POI) around center of the previous frame
-        x= findPixels(frames{frameId}, cy0, a, b);
-        dbu = delta_b_u(frames{frameId}, x, m); %matrix number of pixel in
-                                                % region times m
+    f=figure;
+      imagesc(frames{frameId}), 
+      title(sprintf('Frame %i from %i', frameId, Nframes)), hold on;
+
+    % find center of the target :
+    while 1
+      
+        % plot around current target center
+        t=-pi:0.01:pi;
+            xt= y0(1)+a*cos(t);
+            yt= y0(2)+b*sin(t);
+        plot(xt,yt,'b')
         
-        % Target candidates
-        p_y0 = pdf(x, dbu , cy0, m , h);
-        p_y0_pos = find(p_y0>0)
-        
-%         sprintf('sum(p_y0)=%d', sum(p_y0))
-        % calculate Bhattacharyya coefficient target and target candidate
-        % ro = sum (sqrt(p_y0.*q));
-        
-        q_p = sqrt(q./p_y0);
-        size(q_p);
         % calculate wights
+        q_p = sqrt(q./p_y0);
         w = zeros(1,size(x,1));
+        normXw = zeros(size(x,1),2);
         for i=1:size(x,1)
            w(i) = sum(q_p.*dbu(i,:));
-           x(i,:) = x(i,:)*w(i);
+           normXw(i,:) = normX(i,:).*w(i);
         end  
         
-        % calculate new center candidate
-        cy1 = sum(x)/sum(w);
-          
-        count = 2;
+        % calculate new center 
+        normy1 = sum(normXw)/sum(w);
+        y1 = [round(normy1(1)*a), round(normy1(2)*b)] ;
+        
+%         By numerical problems        
+%         % Target candidates
+%         p_y1 = pdf(normX, dbu , normy1, m , h);
+%         
+%         % Bhattacharyya Coefficient
+%         rho1 = sum(sqrt(q.*p_y1));
+%         
+%         while rho1 <rho0
+%             sprintf(' %f < % f',rho1, rho0 )
+%             y1 = (y0+y1)/2.;
+%             normy1 = [y1(1)/a, y1(2)/b] ;
+%             p_y1 = pdf(normX, dbu , normy1, m , h);
+%             rho1 = sum(sqrt(q.*p_y1));
+%             
+%             t=-pi:0.01:pi;
+%                 xt= y1(1)+a*cos(t);
+%                 yt= y1(2)+b*sin(t);
+%             plot(xt,yt)    
+%         end        
+        
+        if ( sqrt((normy0-normy1)*(normy0-normy1)') < eps)
+            break;
+        else
+            y0 = y1;
+            normy0 = normy1;
+            % Target candidates
+            p_y0 = pdf(normX, dbu , normy0, m , h);
+        end         
     end
-    %
-    cObj(frameId,:) = cy1;
-end
-
-for i=2:Nframes
-    f = figure;
-        imagesc(frames{i});
-        title(sprintf('Frame %i from %i', i, Nframes)), hold on;
-    % plot an ellipse
     t=-pi:0.01:pi;
-        xt= cObj(i,1)+a*cos(t);
-        yt= cObj(i,2)+b*sin(t);
-    plot(xt,yt)
+        xt= y1(1)+a*cos(t);
+        yt= y1(2)+b*sin(t);
+    plot(xt,yt,'g')
     hold off;
-    
     print(f, '-r80', '-dtiff', fullfile(output_path,...
-                                    sprintf('Frame%d.jpg',i)));
+                                     sprintf('Frame%d.jpg',frameId)));
+    %
+    normCT(frameId,:) = normy1;
+    CT(frameId,:) = y1;
 end
+% 
+% for i=2:Nframes
+%     f = figure('visible','off');
+%         imagesc(frames{i});
+%         title(sprintf('Frame %i from %i', i, Nframes)), hold on;
+%     % plot an ellipse
+%     t=-pi:0.01:pi;
+%         xt= CT(i,1)+a*cos(t);
+%         yt= CT(i,2)+b*sin(t);
+%     plot(xt,yt)
+%     hold off;
+%     
+%     print(f, '-r80', '-dtiff', fullfile(output_path,...
+%                                     sprintf('Frame%d.jpg',i)));
+% end
 
+close all;  
 end
 
 %% Function findPixels
@@ -164,7 +193,6 @@ function pixels = findPixels(img, cy, a, b)
 % find pixels inside selected ellipse (x/a)^2+(y/b)^2=1
 
 pixels = [];
-
 for y = cy(2)-b:cy(2)+b % size(img,1)
     for x = cy(1)-a:cy(1)+a %size(img,2)
         dist =  ((x-cy(1))/a)^2 + ((y-cy(2))/b)^2 ;
@@ -175,9 +203,9 @@ for y = cy(2)-b:cy(2)+b % size(img,1)
     end
 end
 
-figure
-    imagesc(img),hold on,
-    plot (pixels(:,1),pixels(:,2),'.b');
+% figure
+%     imagesc(img),hold on,
+%     plot (pixels(:,1),pixels(:,2),'.b');
 
 end
 
@@ -189,13 +217,16 @@ if (size(img,3)~=3)
      error('function b: Input image must be RGB.')
 end
 
-r = img(pixel(1),pixel(2),1)+1;
-g = img(pixel(1),pixel(2),2)+1;
-b = img(pixel(1),pixel(2),3)+1;
 
-r_bin = floor(r/16)+1;
-g_bin = floor(g/16)+1;
-b_bin = floor(b/16)+1;
+r = img(pixel(1),pixel(2),1);
+g = img(pixel(1),pixel(2),2);
+b = img(pixel(1),pixel(2),3);
+
+r_bin = floor(r*16)+1;
+g_bin = floor(g*16)+1;
+b_bin = floor(b*16)+1;
+
+bin = sprintf('%f  %f  %f ', (r_bin-1)*256, (g_bin-1)*16 , b_bin);
 
 bxy = (r_bin-1)*256 + (g_bin-1)*16 + b_bin;
 
@@ -208,23 +239,27 @@ function p = pdf(x, dbu, cy, m, h)
 %       m dimensional of the feature space
     
     % calculate kernel function of pixels
-
-    k_x = zeros(1,size(x,1));
-
+    k_x = double(zeros(1,size(x,1)));
     for i=1:size(x,1)
        k_x(i) = K((x(i,:)-cy)*(x(i,:)-cy)'/h^2);
     end
-
-    C = 1/sum(k_x);  % normalized constant
+    
+    % constant C
+    C = double(1/sum(k_x));  % normalized constant
 
     % probability of the feature u=1..m in the target
-    p = zeros(1,m);
+    p = double(zeros(1,m));
     for u=1:m
        %p(u) =C*sum (k_x(:).*dbu(:,u));
         for i=1:size(x,1)
             p(u) = p(u) +  k_x(i)*dbu(i,u);
         end
         p(u) = p(u)*C;
+        
+        % 
+        if p(u)==0
+           p(u)=0.00001; 
+        end
     end  
     
 end
@@ -232,12 +267,13 @@ end
 %% Kernel with Epanechnikov profile
 function kx = K(x)
 % in our case d = 1, x \in [0, \infty)
-d=1;
-cd = 2*pi ;
+d=1.;
+cd = 2*pi;
+
     if x<=1
-        kx = (d+2)*(1-x)/2/cd;
+       kx = double((d+2)*(1-x))/2./cd;
     else
-        kx = 0;
+        kx = double(0);
     end
 end
 
@@ -245,6 +281,7 @@ end
 function dbu = delta_b_u(img, x, m)
 
     dbu = zeros (size(x,1), m);
+    % dbu = uint16(dbu);
     for i=1:size(x,1)
         b_x = Fbins(img,x(i,:));
         for u=1:m 
@@ -259,4 +296,13 @@ function dbu = delta_b_u(img, x, m)
 %     else
 %         d = 0;
 %     end
+end
+
+%% Normolization of the coordinates
+function [normX, normCenter] = normalizeX(x,y,a,b)
+
+normX(:,1) = x(:,1)./a;
+normX(:,2) = x(:,2)./b;
+
+normCenter = [y(1)/a, y(2)/b];
 end
