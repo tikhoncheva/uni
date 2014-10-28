67804c77db3f8910aae9ff5436dc26d50d7c4278
kasiatikhoncheva@gmail.com <kasiatikhoncheva@gmail.com>
2014-10-29 00:23:08 +0100

    ex02 in process...

--- MachineLeraning/Exercises/ex02/ex02.py
+++ MachineLeraning/Exercises/ex02/ex02.py
@@ -10,7 +10,43 @@ import time
 
 from sklearn.datasets import load_digits
 from sklearn import cross_validation
+# ----------------------------------------------------------------------------    
 
+def NearestMean(x_training, y_training, x_test):
+    
+    nTr, dTr = x_training.shape
+    nTest, dTest = x_test.shape
+    
+    assert dTr==dTest, 'Images in training and test sets have different size'
+
+    labels = np.unique(y_training)
+    nClasses= len(labels)
+    
+    # Compute class mean
+    class_average = np.zeros((nClasses, dTr) , dtype = np.float16)   
+    for l in range(0,nClasses):
+        # for each class 
+        ind = (y_training==labels[l])
+        class_l = x_training[ind,:]
+        nL = len(class_l) # number of elements in the class with label l
+        class_average[l,:] = np.sum(class_l[:,:], axis = 0)/float(nL)
+
+    # end for l
+    
+    prediction = np.zeros(nTest, dtype = np.int8) 
+    
+    for i in range(0,nTest):
+        # for each test image
+        dist = class_average - x_test[i,:]
+        dist = np.sqrt(np.sum(np.square(dist), axis = 1))
+        
+        min_dist = np.argmin(dist)
+        prediction[i] = labels[min_dist]
+    # end for i 
+    return prediction
+# end NearestMean
+# ----------------------------------------------------------------------------    
+    
 # ----------------------------------------------------------------------------    
 # ----------------------------------------------------------------------------    
 def main():
@@ -86,6 +122,8 @@ def main():
     print ' 2 Nearest mean '    
     print '------------------------------------------------------------------'
     
+    y_predict = NearestMean(x_train, y_train, x_test)
+    print 'Test-Predict: {}'. format(y_test-y_predict)
 
         
     return 0
