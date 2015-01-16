8efb0ac95a2bb792cd8e74162f1ad8ddf7fdc489
Ekaterina Tikhoncheva <etikhonc@compvis07.(none)>
2015-01-16 17:47:25 +0100

    first check complited TODO small changes with GUI

--- Master/GUI/ver5/votingSLIC.m
+++ Master/GUI/ver5/votingSLIC.m
@@ -1,38 +1,34 @@
-function [best_neighbors, vals] = votingSCLIC( point, edges, edgeDescr, SP1, SP2, mparam)
+function [best_neighbors, vals, img12SP] = votingSCLIC( point, edges, edgeDescr, SP1, SP2, mparam)
 
-display('Voting');
-
-% figure, imshow(label2rgb(SP1.label,'jet','c','shuffle'));
-
+% selected point
 x = point(1);
 y = point(2);
-
+% label of this point
 point_label = SP1.label(y,x);
 
-[sameSP(:,1), sameSP(:,2)] = find(SP1.label == point_label);
 
-% get descriptor of the point on each scale
+% SP the point belongs to
+[SPxy(:,1), SPxy(:,2)] = find(SP1.label == point_label);
+
+
+% find edge points inside selected SP
 ind = [];
-
-    
-for i=1:size(sameSP,1)
-    SP1.boundary(sameSP(i,1), sameSP(i,2), :) = 0;
-    ind = [ind, find(edges{1}(1,:) == sameSP(i,2) & ...
-                     edges{1}(2,:) == sameSP(i,1)) ]; 
+for i=1:size(SPxy,1)
+    SP1.boundary(SPxy(i,1), SPxy(i,2), 1:2) = 0;  % mark super pixels on the first image    
+    SP1.boundary(SPxy(i,1), SPxy(i,2), 3) = 255;  % mark super pixels on the first image    
+    ind = [ind, find(edges{1}(1,:) == SPxy(i,2) & ...
+                     edges{1}(2,:) == SPxy(i,1)) ]; 
 end
 
-edgeP = edges{1}(1:2,ind);
+edgeSPxy = edges{1}(1:2,ind);
 
-% edgeP = [edgeP'; [x,y]];
-% [edgeP,ia,ic] = unique(edges{1}(1:2,ind));
+SP1.boundary(edgeSPxy(2,:), edgeSPxy(1,:), 1) = 255;
+SP1.boundary(edgeSPxy(2,:), edgeSPxy(1,:), 2:3) = 0;
 
-SP1.boundary(edgeP(2,:),edgeP(1,:), 1) = 255;
-SP1.boundary(edgeP(2,:),edgeP(1,:), 2:3) = 0;
 
-% figure, imshow(SP1.boundary);
-
-neighborlist = []; %zeros(1, numel(ind)*knn);
-vallist = []; %zeros(1, numel(ind)*knn);
+% find kNN nearest neighbors of edge points inside selected SP
+neighborlist = [];
+vallist = []; 
     
 for j = 1:numel(ind)
     % Euclidian distance between descriptors
@@ -47,42 +43,57 @@ for j = 1:numel(ind)
     
     neighborlist = [neighborlist, matches];
     vallist = [vallist, sim];
-
 end
 
+% coordinates of the nearest neighbors
+neighbors_coord = edges{2}(1:2, neighborlist);
 
-labels_coord = edges{2}(1:2, neighborlist);
-linearInd = sub2ind(size(SP2.label), labels_coord(2,:), labels_coord(1,:)); 
-
+% find SP(labels) that contain nearest neighbors
+linearInd = sub2ind(size(SP2.label), neighbors_coord(2,:), neighbors_coord(1,:)); 
 label_list = SP2.label(linearInd);
 
-% for i=1:numel(neighborlist)
-%     point_label2 = SP2.label(edges{2}(2, neighborlist(i)),edges{2}(1,neighborlist(i)));
-%    
-%     [sameSP2x, sameSP2y] = find(SP2.label == point_label2);
-% 
-%     for j=1:size(sameSP2x,1)
-%         SP2.boundary(sameSP2x(j), sameSP2y(j), 1) = 255;
-%         SP2.boundary(sameSP2x(j), sameSP2y(j), 2:3) = 0;
-%     end
-% end
-
-votes_labels = unique(label_list);
-votes_val = zeros(1, numel(votes_labels));
-for i = 1:numel(votes_labels)
-    votes_val(i) = numel( find(label_list==votes_labels(i)));
+% delete multiple SP
+unique_labels = unique(label_list);
+% calculate votes
+votes = zeros(1, numel(unique_labels));
+for i = 1:numel(unique_labels)
+    votes(i) = numel( find(label_list==unique_labels(i)));
 end
-[val, I] = sort(votes_val, 'descend');
 
-best_votes = votes_labels(I(1:mparam.kNN));
+% select top kNN candidates
+[~, I] = sort(votes, 'descend');
+best_votes = unique_labels(I(1:mparam.kNN));
 
 best_neighbors_ID = [];
-for l = 1:numel(best_votes)    
-    best_neighbors_ID = [best_neighbors_ID, find(label_list == best_votes(l) )];
+for i = 1:numel(best_votes)    
+    best_neighbors_ID = [best_neighbors_ID, find(label_list == best_votes(i) )];
 end
 
 best_neighbors = neighborlist(best_neighbors_ID);
 vals = vallist (best_neighbors_ID);
 
+% mark super pixels on the second image
+for i=1:numel(label_list)
+   
+    [sameSP2x, sameSP2y] = find(SP2.label == label_list(i));
+
+    for j=1:size(sameSP2x,1)
+        SP2.boundary(sameSP2x(j), sameSP2y(j), 1:2) = 255;
+        SP2.boundary(sameSP2x(j), sameSP2y(j), 3) = 0;
+    end
+end
+
+for i=1:numel(best_votes)
+   
+    [sameSP2x, sameSP2y] = find(SP2.label == best_votes(i));
+
+    for j=1:size(sameSP2x,1)
+        SP2.boundary(sameSP2x(j), sameSP2y(j), 1) = 255;
+        SP2.boundary(sameSP2x(j), sameSP2y(j), 2:3) = 0;
+    end
+end
+
+img12SP = combine2images(SP1.boundary, SP2.boundary);
+figure, imshow(img12SP);
 
 end
